3ad40672c4bdd6be2a7b96f3b9f47fd557602a9b
MohamedEssaadani <essaadani80@gmail.com>
2022-09-19 16:25:08 +0100

    consume remote functions from remotes in host

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,11 +1,13 @@
 import faker from "faker";
 
+// to mount cart
 const mount = (el) => {
   let cartText = `<div>You have ${faker.random.number()} items in your cart..</div>`;
 
   el.innerHTML = cartText;
 };
 
+// mount isolation mode
 if (process.env.NODE_ENV === "development") {
   let el = document.querySelector("#dev-cart");
   if (el) {
@@ -13,4 +15,5 @@ if (process.env.NODE_ENV === "development") {
   }
 }
 
+// mount in host
 export { mount };
